5695008b01d2c351cba2bef3a04a340429a91113
Mathis <mathun3003@gmail.com>
2023-01-06 10:10:51 +0100

    add literals for user_info & compare_users, add utils strf_datetime func

--- pysna/api.py
+++ pysna/api.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
-from typing import List, Set
+from typing import List, Literal, Set
 
 import tweepy
 from tweepy import API, Client
+
+from pysna.utils import strf_datetime
 
 
 class TwitterAPI(API, Client):
@@ -74,7 +76,18 @@ class TwitterAPI(API, Client):
         tweet_obj = API.get_oembed(self, url=tweet)
         return tweet_obj
 
-    def user_info(self, user: str, attributes: List[str]) -> dict:
+    LITERALS_USER_INFO = Literal['id', 'id_str', 'name', 'screen_name', 'followers_info',
+            'follows_info', 'location', 'profile_location', 'description', 'url', 'entities',
+            'protected', 'followers_count', 'friends_count', 'listed_count', 'created_at',
+            'favourites_count', 'utc_offset', 'time_zone', 'geo_enabled', 'verified', 'statuses_count',
+            'lang', 'status', 'contributors_enabled', 'is_translator', 'is_translation_enabled', 'profile_background_color',
+            'profile_background_image_url', 'profile_background_image_url_https', 'profile_background_tile', 'profile_image_url',
+            'profile_image_url_https', 'profile_banner_url', 'profile_link_color', 'profile_sidebar_border_color',
+            'profile_sidebar_fill_color', 'profile_text_color', 'profile_use_background_image', 'has_extended_profile',
+            'default_profile', 'default_profile_image', 'following', 'follow_request_sent', 'notifications',
+            'translator_type', 'withheld_in_countries']
+
+    def user_info(self, user: str, attributes: List[LITERALS_USER_INFO]) -> dict:
         """Receive requested user information from Twitter User Object.
 
         Args:
@@ -131,7 +144,10 @@ class TwitterAPI(API, Client):
 
         return user_info
 
-    def compare_users(self, user1: str, user2: str, compare: str) -> dict | list:
+    LITERALS_COMPARE_USERS = Literal['num_followers', 'num_follows', 'common_followers',
+            'distinct_followers', 'common_follows', 'distinct_follows', 'created_at']
+
+    def compare_users(self, user1: str, user2: str, compare: LITERALS_COMPARE_USERS) -> dict | list:
         """Compare two users with the specified comparison attribute
 
         Args:
@@ -195,8 +211,8 @@ class TwitterAPI(API, Client):
                 time_difference_in_days = time_difference_in_seconds // 86400
                 time_difference_in_weeks = time_difference_in_days // 7
                 time_difference_in_hours = time_difference_in_seconds // 3600
-                return {f"{user1}_creation_date": user1_obj.created_at,
-                        f"{user2}_creation_date": user2_obj.created_at,
+                return {f"{user1}_creation_date": strf_datetime(user1_obj.created_at),
+                        f"{user2}_creation_date": strf_datetime(user2_obj.created_at),
                         "time_difference_in_seconds": time_difference_in_seconds,
                         "time_difference_in_hours": time_difference_in_hours,
                         "time_difference_in_days": time_difference_in_days,
@@ -205,67 +221,7 @@ class TwitterAPI(API, Client):
             case _:
                 raise ValueError("Invalid comparison attribute for {}".format(compare))
 
-
-        #* alternative using list attribute as input
-
-        # def compare_users(users, compare):
-        #     # Authenticate to Twitter API
-        #     auth = tweepy.OAuth1UserHandler(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        #     api = tweepy.API(auth)
-
-        #     # Get user objects for all users in the list
-        #     user_objs = []
-        #     for user in users:
-        #         user_objs.append(api.get_user(user))
-
-        #     # Compare all users in the list based on the requested attribute
-        #     if compare == 'num_followers':
-        #         # Return the number of followers for each user
-        #         followers = {}
-        #         for user_obj in user_objs:
-        #             followers[user_obj.screen_name] = user_obj.followers_count
-        #         return followers
-        #     elif compare == 'num_follows':
-        #         # Return the number of follows for each user
-        #         follows = {}
-        #         for user_obj in user_objs:
-        #             follows[user_obj.screen_name] = user_obj.friends_count
-        #         return follows
-        #     elif compare == 'common_followers':
-        #         # Find the followers that all users have in common
-        #         all_followers = []
-        #         for user_obj in user_objs:
-        #             all_followers.extend(api.followers_ids(user_obj.screen_name))
-        #         common_followers = set(all_followers)
-        #         for user_obj in user_objs:
-        #             common_followers = common_followers.intersection(api.followers_ids(user_obj.screen_name))
-        #         return list(common_followers)
-        #     elif compare == 'unique_followers':
-        #         # Find the unique followers for each user
-        #         unique_followers = {}
-        #         for user_obj in user_objs:
-        #             user_followers = set(api.followers_ids(user_obj.screen_name))
-        #             for other_user_obj in user_objs:
-        #                 if user_obj.screen_name == other_user_obj.screen_name:
-        #                     continue
-        #                 user_followers = user_followers.difference(api.followers_ids(other_user_obj.screen_name))
-        #             unique_followers[user_obj.screen_name] = list(user_followers)
-        #         return unique_followers
-        #     elif compare == 'created_at':
-        #         # Compare the creation dates of all users
-        #         time_differences = {}
-        #         for i, user1_obj in enumerate(user_objs):
-        #             for j, user2_obj in enumerate(user_objs):
-        #                 if i == j:
-        #                     continue
-        #                 time_difference_in_seconds = (user2_obj.created_at - user1_obj.created_at).total_seconds()
-        #                 time_differences[f"{user1_obj.screen_name} - {user2_obj.screen_name}"] = time_difference_in_seconds
-        #         return time_differences
-        #     else:
-        #         return 'Invalid compare attribute'
-
-
-    #* same with list of users
+    #? same with list of users
     def compare_users_list(self, users: List[str], compare: str) -> dict | list:
         """Compare two or more users with the specified comparison attribute.
 
--- pysna/utils.py
+++ pysna/utils.py
@@ -2,48 +2,61 @@
 import csv
 import json
 import logging
+from datetime import datetime
+from typing import Literal
 
 import pandas as pd
 
 log = logging.getLogger(__name__)
 
-def export_to_file(data: dict, filename: str, type: str, export_path='./'):
+
+def export_to_file(data: dict, filename: str, type: Literal['json', 'csv', 'xlsx'] | None, export_path='./'):
     """_summary_
-    # TODO: fill me
+
     Args:
         data (dict): _description_
         filename (str): _description_
-        type (str): _description_
-        export_path (str): _description_
+        type (Literal[&#39;json&#39;, &#39;csv&#39;, &#39;xlsx&#39;]): _description_
+        export_path (str, optional): _description_. Defaults to './'.
 
     Raises:
         ValueError: _description_
     """
-    # catch Value Errors for 'type'
-    if not type in ['json', 'csv', 'xlsx']:
-        raise ValueError(f"'type' needs to be either json, csv, or xlsx, not {type}.")
-    else:
-        # TODO: test
-        match type:
-            case 'json':
-                try:
-                    with open(export_path + filename + '.json', 'w', encoding='utf-8') as jsonfile:
-                        json.dump(data, jsonfile)
-                except IOError as e:
-                    log.error("I/O error: %s", e)
-            case 'csv':
-                try:
-                    with open(export_path + filename + '.csv', 'w', encoding='utf-8') as csvfile:
-                        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
-                        writer.writeheader()
-                        for entry in data:
-                            writer.writerow(entry)
-                except IOError as e:
-                    log.error("I/O error: %s" % e)
-            case 'xlsx':
-                try:
-                    df = pd.DataFrame(data=data, index=[0])
-                    df.to_excel(export_path + filename + '.xlsx')
-                except IOError as e:
-                    log.error("I/O error: %s" % e)
+
+    # TODO: test
+    match type:
+        case 'json':
+            try:
+                with open(export_path + filename + '.json', 'w', encoding='utf-8') as jsonfile:
+                    json.dump(data, jsonfile)
+            except IOError as e:
+                log.error("I/O error: %s", e)
+        case 'csv':
+            try:
+                with open(export_path + filename + '.csv', 'w', encoding='utf-8') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+                    writer.writeheader()
+                    for entry in data:
+                        writer.writerow(entry)
+            except IOError as e:
+                log.error("I/O error: %s" % e)
+        case 'xlsx':
+            try:
+                df = pd.DataFrame(data=data, index=[0])
+                df.to_excel(export_path + filename + '.xlsx')
+            except IOError as e:
+                log.error("I/O error: %s" % e)
     pass
+
+
+def strf_datetime(date: datetime, format="%Y-%m-%d %H:%M:%S") -> str:
+    """Convert datetime object to string representation.
+
+    Args:
+        date (datetime): Input datetime object
+        format (str, optional): Datetime string format. Defaults to "%Y-%m-%d %H:%M:%S".
+
+    Returns:
+        str: string representation of input datetime in given format.
+    """
+    return date.strftime(format)
