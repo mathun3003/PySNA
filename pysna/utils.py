a2f1bae658821bd76be7b9036282b4dbeed51f9a
Mathis <mathun3003@gmail.com>
2023-01-18 14:02:00 +0100

    debug export_to_file, append_to_file

--- pysna/utils.py
+++ pysna/utils.py
@@ -15,45 +15,48 @@ log.setLevel(logging.ERROR)
 # log to stdout
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 log.addHandler(handler)
 
 
-def export_to_file(data: dict, export_path: str, type: Literal['json', 'csv', 'xlsx'] | None, encoding: str = "utf-8"):
+def export_to_file(data: dict, export_path: str, type: Literal["json", "csv", "xlsx"], encoding: str = "utf-8", *args):
     """_summary_
-
+    TODO: fill
     Args:
         data (dict): _description_
-        filename (str): _description_
-        type (Literal[&#39;json&#39;, &#39;csv&#39;, &#39;xlsx&#39;]): _description_
-        export_path (str, optional): _description_.
-
-    Raises:
-        ValueError: _description_
+        export_path (str): _description_
+        type (str, optional): _description_. Defaults to 'json' | 'csv' | 'xlsx'.
+        encoding (str, optional): _description_. Defaults to "utf-8".
     """
 
     # TODO: test
     match type:
-        case 'json':
+        # export to json
+        case "json":
             try:
-                with open(export_path, 'w', encoding=encoding) as jsonfile:
-                    json.dump(data, jsonfile)
+                with open(export_path, "w", encoding=encoding) as jsonfile:
+                    # add 'data' key in order to append additional dicts to same file
+                    data = {"data": [data]}
+                    # dump to json
+                    json.dump(data, jsonfile, indent=4, *args)
             except IOError as e:
                 log.error("I/O error: %s", e)
-        case 'csv':
+        # export to csv
+        case "csv":
             try:
-                with open(export_path, 'w', encoding=encoding) as csvfile:
-                    writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+                with open(export_path, "w", encoding=encoding) as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=data.keys(), *args)
                     writer.writeheader()
                     for entry in data:
                         writer.writerow(entry)
             except IOError as e:
                 log.error("I/O error: %s" % e)
-        case 'xlsx':
+        # export to excel
+        case "xlsx":
             try:
                 df = pd.DataFrame(data=data, index=[0])
-                df.to_excel(export_path)
+                df.to_excel(export_path, *args)
             except IOError as e:
                 log.error("I/O error: %s" % e)
     pass
@@ -65,7 +68,7 @@ def append_to_file(input_dict: Dict[str, Any], filepath: str, file_type: str = "
     Args:
         input_dict (Dict[str, Any]): Dictionary containing new data that should be added to file.
         filepath (str): Absolute or relative filepath including the file extension. Depending on the current working directory.
-        file_type (str, optional): Whether to extend a CSV or JSON file. Defaults to "json".
+        file_type (str, optional): Whether to extend a CSV or JSON file. Options: "csv", "json". Defaults to "json".
         timestamp_column (str | None, optional): The timestamp column of the CSV file that should be parsed during read-in. Defaults to None.
         encoding (str, optional): The encoding of the file. Defaults to "utf-8".
 
@@ -78,25 +81,19 @@ def append_to_file(input_dict: Dict[str, Any], filepath: str, file_type: str = "
     match file_type:
         case "json":
             # load file from path
-            with open(filepath, 'r', encoding=encoding) as input_file:
-                f = json.loads(input_file)
-            # check if both dicts have the same keys. If not, raise an exception.
-            if not set(list(input_dict.keys())) == set(list(f.keys())):
-                raise ValueError("Unknown key(s). Make sure that the input dictionary and the JSON file have the same keys.")
+            with open(filepath, "r", encoding=encoding) as input_file:
+                f = json.load(input_file)
+            # existing file should have a "data"-key and a list to append to
+            if "data" not in f.keys():
+                raise KeyError("The file to be extended must contain the key 'data'.")
             else:
-                #? really necessary? The same timestamp is unlikely to occur twice in a file.
-                # check if timestamp already exist in input file
-                # if input_dict["timestamp"] in f.keys():
-                #     # check if ID already exists in input file
-                #     input_dict["id"] in ...
-
-                # delete utc_timestamp entry from input dict
-                input_dict.pop("utc_timestamp")
-                # insert new entries to existing file from input dict
-                f[input_dict["utc_timestamp"]] = input_dict
-                # write to file, overwrite
-                with open(filepath, 'w', encoding=encoding) as jsonfile:
-                    json.dump(f, jsonfile)
+                try:
+                    # append new dict to file
+                    f["data"].append(input_dict)
+                    with open(filepath, "w", encoding=encoding) as jsonfile:
+                        json.dump(f, jsonfile, indent=4)
+                except IOError as e:
+                    raise e
         case "csv":
             # read CSV file from path
             f = pd.read_csv(filepath, encoding=encoding, **kwargs, parse_dates=timestamp_column)
@@ -109,7 +106,7 @@ def append_to_file(input_dict: Dict[str, Any], filepath: str, file_type: str = "
                 # export file, overwrite
                 f.to_csv(filepath, encoding=encoding, index=False)
         case _:
-            log.error("I/O error: %s" % IOError("Invalid file type provided for {}".format(file_type)))
+            raise IOError("Invalid file type provided for {}".format(file_type))
     pass
 
 
